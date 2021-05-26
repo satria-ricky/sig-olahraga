65a29a5fd1fdc7828d22a3057566a2c2e5a787dc
satria-ricky <rickygodhand@gmail.com>
2021-05-26 00:55:54 -0700

    laporan

--- application/models/M_profile.php
+++ application/models/M_profile.php
@@ -7,9 +7,6 @@ class M_profile extends CI_model {
         $this->db->update('tb_admin', $data, array('admin_id' => $id));
     }
 
-    public function get_nama_bidang($id){
-        return $this->db->get_where('tb_bidang',  ['bidang_id' => $id])->row_array();
-    }
 
     public function cek_username($username, $id){
         $sql='SELECT * FROM tb_admin where admin_username = ? AND admin_id != ?';
